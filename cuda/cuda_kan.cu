18287f30750d2ca1e6f32f317990b156b049e7aa
mirodavide <filomenodavide.miro@gmail.com>
2024-10-13 00:26:13 +0200

    Fix CUDA compilation errors

--- cuda/cuda_kan.cu
+++ cuda/cuda_kan.cu
@@ -21,7 +21,7 @@ using namespace std;
 
 namespace cuda_kan {
 
-    __device__ float silu(float *x, int i) {
+    __device__ float silu(float x) {
         return 1 / (1 + expf(x[i] * -1));
 
     }
