783524386733d55b25920d0affe218b38348807b
mirodavide <filomenodavide.miro@gmail.com>
2024-11-19 22:56:19 +0100

    fix: knots dimension, spline function

--- cuda/cuda_kan.cu
+++ cuda/cuda_kan.cu
@@ -27,17 +27,17 @@ namespace cuda_kan {
         return x / (1 + expf(x * -1));
     }
 
-    __global__ void kan_activation_function(float **x, float **y, const float **wb, const float **ws, const float **cps, float ****b_spline_basis, int k, int batch_size, int num_inputs, int num_activations) {
+    __global__ void kan_activation_function(float **x, float **y, const float **wb, const float **ws, const float **cps, float ****b_spline_basis, int k, int batch_size, int num_inputs, int num_activations, int num_knots) {
 
         int z = blockIdx.x * blockDim.x + threadIdx.x;
         int i = blockIdx.x * blockDim.x + threadIdx.y;
         int j = blockIdx.x * blockDim.x + threadIdx.z;
 
-
         float result = 0.0;
+
         if (i < num_inputs && z < batch_size && j < num_activations) {
-            //TODO: check cps dimensions and b_spline
-            result = wb[i][j] * silu(x[z][i]) + ws[i][j] * cps[i][j] * b_spline_basis[z][i][j][k];
+            spline<<<1,num_knots>>>(&result, cps, b_spline_basis, z, i, j, d, num_knots);
+            result = result + wb[i][j] * silu(x[z][i]) + ws[i][j];
             atomicAdd(&y[z][j], result);
         }
 
@@ -49,8 +49,8 @@ namespace cuda_kan {
          * x : [batch_size, input_dim]
          * y : [batch_size, output_dim]
          * wb,ws: [input_dim, output_dim]
-         * cps : [input_dim, num_knots]
-         * knots : [num_knots]
+         * cps : [output_dim, num_knots]
+         * knots : [input_dim, num_knots]
          */
 
         TORCH_CHECK(wb.size(0) < MAX_DIM); //TODO: review check
@@ -73,6 +73,7 @@ namespace cuda_kan {
         int batch_size = x.size(0);
         int num_input = x.size(1);
         int num_activations = wb.size(0);
+        int num_knots = cps.size(1);
 
 
         at::Tensor x_contig = x.contiguous();
@@ -96,17 +97,14 @@ namespace cuda_kan {
         float **y_ptr = y.data_ptr<float*>();
         float ****b_spline_basis_ptr = b_spline_basis.data_ptr<float***>();
 
-        int num_block = (batch_size / MAX_THD) + 1;
-        int num_threads = MAX_THD;
-
-        b_spline_base<<num_block, num_threads>>(b_spline_basis_ptr, x_ptr, batch_size, num_input, num_activations, degree, knots_ptr);
-
-
         int dim = MAX_DIM / 3;
+        int num_block = max(batch_size, num_input);
+        dim3 threads_block(min(dim + 1,batch_size),min(dim,num_input)); // batch_size x num_input
+        b_spline_base<<<num_block, threads_block>>>(b_spline_basis_ptr, x_ptr, batch_size, num_input, num_activations, degree, knots_ptr);
+
+
         num_block = max(batch_size,max(num_input,num_activations));
         dim3 threads_block(min(dim + 1,batch_size),min(dim,num_input),min(dim,num_activations)); // batch_size x num_input x num_activations
-
-
         kan_activation_function<<<num_block, threads_block>>>(x_ptr, y_ptr, wb_ptr, ws_ptr, cps_ptr, b_spline_basis_ptr, degree, batch_size, num_input, num_activations);
 
         return y;
--- cuda/spline.cu
+++ cuda/spline.cu
@@ -9,7 +9,14 @@
 
 using namespace std;
 
-__global__ void b_spline_base(float**** b_spline_basis, float** x, int batch_size, int num_input, int num_activations, int degree,const float* knots) {
+__global__ void b_spline_base(float**** b_spline_basis, float** x, int batch_size, int num_input, int num_knots, int degree,const float** knots) {
+    /*
+     * z : z-th batch element
+     * i : i-th element of the input
+     * j : j-th knot
+     * k : degree
+     */
+
     int z = blockIdx.x * blockDim.x + threadIdx.x;
     int i = blockIdx.x * blockDim.x + threadIdx.y;
     float t;
@@ -21,12 +28,12 @@ __global__ void b_spline_base(float**** b_spline_basis, float** x, int batch_siz
     }
 
     for(int d = 0; d < degree; d++) {
-        for (int j = 0; j < num_activations; i++) {
+        for (int j = 0; j < num_knots; j++) {
 
             t = x[z][i];
             if (d == 0) {
                 // Base case: piecewise constant function (degree 0)
-                if (knots[j] <= t && t < knots[j + 1]) {
+                if (knots[i][j] <= t && t < knots[i][j + 1]) {
                     b_spline_basis[z][i][j][d] = 1.0;
                 } else {
                     b_spline_basis[z][i][j][d] = 0.0;
@@ -35,14 +42,13 @@ __global__ void b_spline_base(float**** b_spline_basis, float** x, int batch_siz
 
 
                 // Check the left term (avoid division by zero)
-                if (knots[j + d] != knots[j]) {
-                    leftTerm = (t - knots[j]) / (knots[j + d] - knots[j]) * b_spline_basis[z][i][j][d - 1];
+                if (knots[i][j + d] != knots[i][j]) {
+                    leftTerm = (t - knots[i][j]) / (knots[i][j + d] - knots[i][j]) * b_spline_basis[z][i][j][d - 1];
                 }
 
                 // Check the right term (avoid division by zero)
-                if (knots[j + d + 1] != knots[j + 1]) {
-                    rightTerm = (knots[j + d + 1] - t) / (knots[j + d + 1] - knots[j + 1]) *
-                                b_spline_basis[z][i][j + 1][d - 1];
+                if (knots[i][j + d + 1] != knots[i][j + 1]) {
+                    rightTerm = (knots[i][j + d + 1] - t) / (knots[i][j + d + 1] - knots[i][j + 1]) * b_spline_basis[z][i][j + 1][d - 1];
                 }
 
                 b_spline_basis[z][i][j][d] = leftTerm + rightTerm;
@@ -53,15 +59,17 @@ __global__ void b_spline_base(float**** b_spline_basis, float** x, int batch_siz
     }
 }
 
-__global__ void spline(float* result, const float** cps, const float* knots, const float**** b_spline_basis, int i, int k){
-    int z = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.x * blockDim.x + threadIdx.y;
+__global__ void spline(float* result, const float** cps, const float**** b_spline_basis, int z, int i, int j, int d, int num_knots) {
+    int k = blockIdx.x * blockDim.x + threadIdx.x;
     /*
      * z : z-th batch element
      * i : i-th element of the input
      * j : j-th activation function
-     * k : degree
+     * k : k-th knot
+     * d : degree
      */
-    atomicAdd(&result, cps[j] * b_spline_basis[z][i][j][k]);
+    if (k < num_knots) {
+        atomicAdd(&result, cps[j][k] * b_spline_basis[z][i][k][d]);
+    }
 }
 
