14ab56dcc5eda119d6684a80ee5152182cd30e90
mirodavide <filomenodavide.miro@gmail.com>
2024-11-24 21:33:14 +0100

    feat: remove dynamic parallelism, COMPILATION OK

--- cpp/cpp_kan.cpp
+++ cpp/cpp_kan.cpp
@@ -8,6 +8,7 @@
 #include <ATen/ATen.h>
 
 
+#include "spline.cpp"
 
 #define MAX_DIM 1024
 #define MAX_THD 1024
@@ -22,71 +23,6 @@ namespace cpp_kan {
 
     }
 
-    void b_spline_base(float**** b_spline_basis, float** x, int batch_size, int num_input, int num_knots, int degree,float** knots) {
-        /*
-         * z : z-th batch element
-         * i : i-th element of the input
-         * j : j-th knot
-         * k : degree
-         */
-
-
-        float t;
-        double leftTerm = 0.0;
-        double rightTerm = 0.0;
-
-        for(int z = 0; z < batch_size; z++) {
-            for (int i = 0; i < num_input; i++) {
-                for (int d = 0; d < degree; d++) {
-                    for (int j = 0; j < num_knots; j++) {
-
-                        t = x[z][i];
-                        if (d == 0) {
-                            // Base case: piecewise constant function (degree 0)
-                            if (knots[i][j] <= t && t < knots[i][j + 1]) {
-                                b_spline_basis[z][i][j][d] = 1.0;
-                            } else {
-                                b_spline_basis[z][i][j][d] = 0.0;
-                            }
-                        } else {
-
-
-                            // Check the left term (avoid division by zero)
-                            if (knots[i][j + d] != knots[i][j]) {
-                                leftTerm = (t - knots[i][j]) / (knots[i][j + d] - knots[i][j]) *
-                                           b_spline_basis[z][i][j][d - 1];
-                            }
-
-                            // Check the right term (avoid division by zero)
-                            if (knots[i][j + d + 1] != knots[i][j + 1]) {
-                                rightTerm = (knots[i][j + d + 1] - t) / (knots[i][j + d + 1] - knots[i][j + 1]) *
-                                            b_spline_basis[z][i][j + 1][d - 1];
-                            }
-
-                            b_spline_basis[z][i][j][d] = leftTerm + rightTerm;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    float spline(float** cps, float**** b_spline_basis, int z, int i, int j, int d, int num_knots) {
-        /*
-         * z : z-th batch element
-         * i : i-th element of the input
-         * j : j-th activation function
-         * k : k-th knot
-         * d : degree
-         */
-        float result = 0.0;
-        for(int k = 0; k < num_knots; k++){
-            result += cps[j][k] * b_spline_basis[z][i][k][d];
-        }
-
-        return result;
-    }
-
 
     void kan_activation_function(float **x, float **y, float **wb, float **ws, float **cps, float ****b_spline_basis, int k, int batch_size, int num_inputs, int num_activations, int num_knots) {
 
@@ -99,8 +35,6 @@ namespace cpp_kan {
         }
 
     }
-
-
 
 
     at::Tensor kan_layer(at::Tensor x, at::Tensor wb, at::Tensor ws, at::Tensor knots, at::Tensor cps, int64_t degree) {
