89ede80eec31147c05ca2c41a856bed6e9c06148
mirodavide <filomenodavide.miro@gmail.com>
2024-12-31 01:14:11 +0100

    fix: dynamic parallelism linking error

--- cuda/setup.py
+++ cuda/setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(name='cuda_kan',
-      ext_modules=[CUDAExtension('cuda_kan', ['cuda_kan.cu'], extra_compile_args={
-            'nvcc': ['-rdc=true']  # Enable relocatable device code
+      ext_modules=[CUDAExtension('cuda_kan',
+                        ['cuda_kan.cu', 'spline.cu'],
+                        dlink=True,
+                        dlink_libraries=["dlink_lib"],
+                                 extra_compile_args={
+                                       'nvcc': ['-rdc=true']  # Enable relocatable device code
       })],
       cmdclass={'build_ext': BuildExtension})
