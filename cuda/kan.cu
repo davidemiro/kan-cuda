1679c29896fda254489331e64d9639328c9337d4
mirodavide <filomenodavide.miro@gmail.com>
2025-01-02 22:55:14 +0100

    fix: thread and thread blocks distribution

--- cuda/kan.cu
+++ cuda/kan.cu
@@ -96,15 +96,12 @@ namespace cuda_kan {
         float *y_ptr = y.data_ptr<float>();
         float *b_spline_basis_ptr = b_spline_basis.data_ptr<float>();
 
-        int dim = MAX_DIM / 3;
-        int num_block = max(batch_size, num_input);
-        dim3 threads_block(min(dim + 1,batch_size),min(dim,num_input)); // batch_size x num_input
-        b_spline_base<<<num_block, threads_block>>>(b_spline_basis_ptr, x_ptr, DIMS, knots_ptr);
+
+        b_spline_base<<<batch_size, num_input>>>(b_spline_basis_ptr, x_ptr, DIMS, knots_ptr);
         cudaDeviceSynchronize();
 
-        num_block = max(batch_size,max(num_input,num_activations));
-        dim3 threads_block_(min(dim + 1,batch_size),min(dim,num_input),min(dim,num_activations)); // batch_size x num_input x num_activations
-        kan_activation_function<<<num_block, threads_block_>>>(x_ptr, y_ptr, wb_ptr, ws_ptr, cps_ptr, b_spline_basis_ptr, degree, batch_size, num_input, num_activations, num_knots);
+        dim3 threads_block(num_input,num_activations); // batch_size x num_input x num_activations
+        kan_activation_function<<<batch_size, threads_block>>>(x_ptr, y_ptr, wb_ptr, ws_ptr, cps_ptr, b_spline_basis_ptr, degree, batch_size, num_input, num_activations, num_knots);
         cudaDeviceSynchronize();
 
         return y;
