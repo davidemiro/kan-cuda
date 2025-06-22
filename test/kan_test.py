21da8fc8c6f4af178c1a6f507e505d6a898c4820
mirodavide <filomenodavide.miro@gmail.com>
2025-06-22 22:48:02 +0200

    fix: define time test

--- test/kan_test.py
+++ test/kan_test.py
@@ -21,7 +21,7 @@ class KolmogorovArnoldNetworkTests(unittest.TestCase):
         layer = kan_impl.KolmogorovArnoldLayer(input_dim=input_dim,output_dim=output_dim,num_knots=grid, range_knots=[0,100], degree=k, device=device)
 
 
-        official_layer = kan.KAN(width=[output_dim], grid=grid, k=k, seed=42, device=device)
+        official_layer = kan_impl.KAN(width=[output_dim], grid=grid, k=k, seed=42, device=device)
 
         start = time.time();
         layer(x)
